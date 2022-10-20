7556c2ee7042fdab1a8fe7d830212b9713e28d6d
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-10-20 21:37:44 +0700

    Continue working Lesson002

--- src/Java/Lesson002/FirstTask.java
+++ src/Java/Lesson002/FirstTask.java
@@ -1,11 +1,31 @@
 package Java.Lesson002;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FirstTask {
-    public List<List<Integer>> combine(int n, int k) {
-        for (int i = 1; i < n; i++) {
+    public static List<List<Integer>> combine(int n, int k) {
+        List<Integer> pattern = new ArrayList<>();
+        for (int i = 1; i <= k; i++) pattern.add(i);
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(List.copyOf(pattern));
+        int anchor = k - 1;
+        int i = k;
+         do {
+            if (i != n) {
+                i++;
+                pattern.set(anchor, i);
+                result.add(List.copyOf(pattern));
+            }
+            else {
+                anchor--;
+                i = pattern.get(anchor);
+            }
+        } while (i != n - k);
+        return result;
+    }
 
-        }
+    public static void main(String[] args) {
+        System.out.println(combine(4,2));
     }
 }
